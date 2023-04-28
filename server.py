dc4ca306c0af091301aa249e4018629f4fad6a0e
ricardoposada <32101942+ricardoposada@users.noreply.github.com>
2023-04-28 17:24:19 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,34 @@
+#! /usr/bin/python3
 import socket
 import sqlite3
 
 def updateScore(node, val):
     dbCon = sqlite3.connect("trustScores.db")
     cur = dbCon.cursor()
-    statement = "UPDATE scores SET score = " + val + " WHERE node = '" + node +"'"
-    cur.execute(statement)
-    rows = cur.execute("SELECT * FROM scores").fetchall()
-    print(rows)
-    dbCon.commit()
-    cur.close()
-    dbCon.close()
+
+    res = cur.execute("SELECT * FROM scores WHERE node='" + node + "'")
+    if res.fetchone() is None:
+        statement = "INSERT INTO scores VALUES('{}', {})".format(node, val)
+        cur.execute(statement)
+        rows = cur.execute("SELECT * FROM scores").fetchall()
+        print(rows)
+        dbCon.commit()
+        cur.close()
+        dbCon.close()
+        return
+    else:
+        statement = "UPDATE scores SET score = " + val + " WHERE node = '" + node +"'"
+        cur.execute(statement)
+        rows = cur.execute("SELECT * FROM scores").fetchall()
+        print(rows)
+        dbCon.commit()
+        cur.close()
+        dbCon.close()
 
 
 def queryScore(node, netCon):
     dbCon = sqlite3.connect("trustScores.db")
     cur = dbCon.cursor()
-
-
 
     statement = "SELECT score FROM scores WHERE node = '" + node + "'"
     rows = cur.execute(statement).fetchall()
@@ -32,13 +43,11 @@ def queryScore(node, netCon):
     netCon.sendall(bytes(data, 'utf-8'))
 
 
-
 if __name__ == '__main__':
 
-#the database needs to be created manually once before the server can function normally. I've included some commands in here for convenience that can be used
-#just to initialize the database, add a test value, and check that its working. Dont run these commands every time the server runs, I usually just run them in the 
-#python shell
-
+    # the database needs to be created manually once before the server can function normally. I've included some commands in here for convenience that can be used
+    # just to initialize the database, add a test value, and check that its working. Dont run these commands every time the server runs, I usually just run them in the
+    # python shell
     #dbCon = sqlite3.connect("trustScores.db")
     #cur = dbCon.cursor()
     #cur.execute("CREATE TABLE scores(node TEXT, score INTEGER)")
@@ -49,7 +58,7 @@ if __name__ == '__main__':
     #cur.close()
     #dbCon.close()
 
-#HOST is the IP of the vm running the server, i.e. the THA Peer
+    # HOST is the IP of the vm running the server, i.e. the THA Peer
     HOST = '192.168.6.3'
     PORT = 34493
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
